af6f500d3c1a4ca7c7ff4e70b40ea857a5486d50
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-06-28 20:11:09 +0300

    feat: add article component

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,16 +1,15 @@
-import './article.scss';
+import "./article.scss";
+import { trimText, formatArticleDate } from "../utils/utils";
 
-export function Article() {
-
+export function Article({ article }) {
   return (
     <article className="article">
-      <p className="article__date">12 июня 2022</p>
-      <h3 className="article__title">Заголовок статьи</h3>
-      <p className="article__text">
-        Краткое описание статьи или ее начало. Может занимать больше трех строк в высоту. В таком случае предложение заканчивается многоточием...
-      </p>
-      <button className="article__btn">Иван Иванов</button>
+      <p className="article__date">{formatArticleDate(article.publishedAt)}</p>
+      <h3 className="article__title">{article.title}</h3>
+      <p className="article__text">{trimText(article.description, 180)}</p>
+      <button className="article__btn">
+        {article.author ? article.author : "Unknown"}
+      </button>
     </article>
   );
 }
-
