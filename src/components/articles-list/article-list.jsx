40ffec8b5947c05e45f01dc380e4b0d29d8c9a43
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-06-28 20:10:52 +0300

    feat: add article list component

--- src/components/articles-list/article-list.jsx
+++ src/components/articles-list/article-list.jsx
@@ -1,17 +1,22 @@
-import './articles-list.scss';
-import { Article } from '../article/article';
+import "./articles-list.scss";
+import { Article } from "../article/article";
+import { Spinner } from "../spinner/spinner";
 
-export function ArticleList() {
+export function ArticleList({ isDataLoaded, articles, setFilters, filters }) {
+  if (!isDataLoaded) {
+    return <Spinner />;
+  }
 
   return (
     <ul className="articles-list">
-      <li><Article /></li>
-      <li><Article /></li>
-      <li><Article /></li>
-      <li><Article /></li>
-      <li><Article /></li>
-      <li><Article /></li>
+      {articles.map((article) => (
+        <li
+          key={article.title}
+          onClick={() => setFilters({ ...filters, author: article.author })}
+        >
+          <Article article={article} />
+        </li>
+      ))}
     </ul>
   );
 }
-
